acfc6bdfb9f1da39331c0268641c790d4bad36e1
ktorz <matthias.benkort@gmail.com>
2015-11-19 10:10:20 +0100

    Create app structure and try webpack

---
+++ frontend/src/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Redux from 'redux';
+import SocketIO from 'socket.io-client';
+import ReactRedux from 'react-redux';
+
+ReactDOM.render(
+    <div>Hexode</div>,
+    document.getElementById('app')
+);
