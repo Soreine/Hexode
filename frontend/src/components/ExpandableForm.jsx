42abc3efa9c4557969282f514ddf401554b8cb84
ktorz <matthias.benkort@gmail.com>
2015-11-29 23:09:10 +0100

    Rewrite ExpandableForm with an internal state + add missing bindings

--- frontend/src/components/ExpandableForm.jsx
+++ frontend/src/components/ExpandableForm.jsx
@@ -1,39 +1,65 @@
 import React from 'react'
 import VerifInput from './VerifInput'
+import Div from './Div'
 import '../styles/components/expandable_form.scss'
 
 export default React.createClass({
-    handleClick: function (e) {
-        e.preventDefault()
-        this.props.onExpand(this.props.expanded)
+    getInitialState() {
+        return { expanded: false }
     },
-    handleLogin: function (e) {
+
+    toggleExpand(e) {
+        e.preventDefault()
+        this.setState({ expanded: !this.state.expanded })
+    },
+
+    handleLogin(e) {
         e.preventDefault()
         this.props.onLogin(this.refs.username.value, this.refs.password.value)
     },
-    handleRegister: function (e) {
 
+    handleRegister(e) {
+        e.preventDefault()
+        this.props.onRegister(
+            this.refs.username.value,
+            this.refs.password.value,
+            this.refs.passwordbis.value
+        )
     },
+
     render() {
-        const { expanded, disabled } = this.props
-        const passwordBis = (<VerifInput ref="passwordbis" type="password" placeholder="password (again)" />)
+        const { disabled } = this.props
+        const { expanded } = this.state
+
+        const passwordBis = (
+            <VerifInput
+                ref="passwordbis"
+                type="password"
+                placeholder="password (again)" />
+        )
 
         return (
-            <div className="exp-form-wrapper">
-                <form onSubmit = { expanded ? this.handleRegister : this.handleLogin }>
+            <Div className="exp-form-wrapper" content = (
+                <form
+                    onSubmit = { expanded ? this.handleRegister : this.handleLogin }>
                     <VerifInput ref="username" placeholder="username" />
-                    <VerifInput ref="password" type="password" placeholder="password" />
-                        { expanded && passwordBis }
+                    <VerifInput
+                        ref="password"
+                        type="password"
+                        placeholder="password"
+                    />
+                    { expanded && passwordBis }
                     <input
                         type = "submit"
                         disabled = { disabled }
                         value = { expanded ? "Register" : "Login" }
                     />
                 </form>
-                    <a onClick={this.handleClick}>
+                    <a
+                        onClick={this.toggleExpand}>
                         { expanded ? "Back to login" : "Create an account" }
                     </a>
-            </div>
+            )/>
         )
     }
 })
