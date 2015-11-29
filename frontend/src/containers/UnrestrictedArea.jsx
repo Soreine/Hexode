8f23411916cce6a38cbf40736f634fe76911d125
ktorz <matthias.benkort@gmail.com>
2015-11-29 23:35:51 +0100

    Add logic to handle login and registration within unrestricted area container

--- frontend/src/containers/UnrestrictedArea.jsx
+++ frontend/src/containers/UnrestrictedArea.jsx
@@ -1,12 +1,36 @@
 import React from 'react'
 import { connect } from 'react-redux'
 
+import * as actions from '../actions/unrestricted_area'
+import ExpandableForm from '../components/ExpandableForm'
+
 const RestrictedArea = React.createClass({
+    handleFormLogin(...credentials) {
+        this.props.dispatch(actions.loginQuery(...credentials))
+    },
+
+    handleFormRegister(...credentials) {
+        this.props.dispatch(actions.registerQuery(...credentials))
+    },
+
     render() {
+        const { fetching, validations } = this.props
+
         return (
-            <div>Unrestricted Area</div>
+            <ExpandableForm
+                disabled={fetching}
+                onLogin={this.handleFormLogin}
+                onRegister={this.handleFormRegister}
+            />
         )
     }
 })
 
-export default connect(state => state)(RestrictedArea)
+const select = state => {
+    return {
+        fetching: state.common.fetching,
+        validations: state.common.validations
+    }
+}
+
+export default connect(select)(RestrictedArea)
