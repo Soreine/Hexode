88ef816cf1a8bc6420ba7737754d293dc1dca386
ktorz <matthias.benkort@gmail.com>
2015-12-01 00:21:46 +0100

    Create lobby container

--- frontend/src/containers/Lobby.jsx
+++ frontend/src/containers/Lobby.jsx
@@ -1,15 +1,44 @@
 import React from 'react'
 import { connect } from 'react-redux'
 
+import * as lobby from '../actions/lobby'
+
 const Lobby = React.createClass({
+    logout(e) {
+        e.preventDefault()
+        this.props.dispatch(lobby.logoutQuery())
+    },
+
+    componentDidMount() {
+        this.props.dispatch(lobby.gamesQuery())
+    },
+
     render() {
-        return (<div>Lobby</div>)
+        const { username, games } = this.props
+
+        const renderGames = games => {
+            return games.map((x, i) => {
+                return (<div key={i}>{x.name}</div>)
+            })
+        }
+
+        return (<div>
+            <p>Welcome {username} !</p>
+            <a href="#" onClick={this.logout}>logout</a>
+            <div>
+                {renderGames(games)}
+            </div>
+        </div>)
     }
 })
 
 function select (state) {
-    return {}
+    return {
+        username: state.unrestricted_area.user.username,
+        games: state.lobby.games,
+        joinRequest: state.lobby.joinRequest,
+        createRequest: state.lobby.createRequest
+    }
 }
 
 export default connect(select)(Lobby)
-
