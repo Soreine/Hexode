c50887e3ee66ccce11281203e77147b9ae6b44be
ktorz <matthias.benkort@gmail.com>
2015-12-07 22:18:46 +0100

    Define config used in utils

--- backend/src/config.js
+++ backend/src/config.js
@@ -1,6 +1,7 @@
 var CONFIG = {
     //Add Static conf values here
+    EXPIRATION_DELAY: 1000 * 60 * 60 * 24 * 30, // 30 days
+    SECRET_KEY: process.env.SECRET_KEY || "developmentKey"
 }
 
-
-module.exports = CONFIG;
+module.exports = CONFIG
