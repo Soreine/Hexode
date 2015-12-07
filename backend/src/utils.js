5ff05ee0631a70f58725a5cfc0ebc37a73f4c340
ktorz <matthias.benkort@gmail.com>
2015-12-07 22:18:30 +0100

    Add and define utils

---
+++ backend/src/utils.js
@@ -0,0 +1,17 @@
+const CONFIG = require('./config')
+
+/** () -> String */
+exports.UUID = function UUID() {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        const r = crypto.randombytes(1)[0]%16|0
+        return (v = c === 'x' ? r : r&0x3|0x8).toString(16)
+    })
+}
+
+/** User -> String */
+exports.genToken = function genToken(user) {
+    return crypto
+        .createCipher('aes256', CONFIG.SECRET_KEY)
+        .update(user.id + "|" + (Date.now() + CONFIG.EXPIRATION_DELAY))
+        .digest('base64')
+}
