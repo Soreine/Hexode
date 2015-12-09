2d23566162cd4ea9c7ebdadd36706a233ea7dc8f
ktorz <matthias.benkort@gmail.com>
2015-12-09 23:21:27 +0100

    Fix typo in utils

--- backend/src/utils.js
+++ backend/src/utils.js
@@ -4,15 +4,14 @@ const crypto = require('crypto')
 /** () -> String */
 exports.UUID = function UUID() {
     return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-        const r = crypto.randombytes(1)[0]%16|0
-        return (v = c === 'x' ? r : r&0x3|0x8).toString(16)
+        const r = crypto.randomBytes(1)[0]%16|0
+        return (c === 'x' ? r : r&0x3|0x8).toString(16)
     })
 }
 
 /** User -> String */
 exports.genToken = function genToken(user) {
-    return crypto
-        .createCipher('aes256', CONFIG.SECRET_KEY)
-        .update(user.id + "|" + (Date.now() + CONFIG.EXPIRATION_DELAY))
-        .digest('base64')
+    let cypher = crypto.createCipher('aes256', CONFIG.SECRET_KEY)
+    cypher.update(user.id + "|" + (Date.now() + CONFIG.EXPIRATION_DELAY), 'utf8', 'base64')
+    return cypher.final('base64')
 }
