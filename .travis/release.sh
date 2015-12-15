841f292a04343df1a09ab54d15ca7ed4da877043
ktorz <matthias.benkort@gmail.com>
2015-12-15 21:38:09 +0100

    Add some log in release.sh

--- .travis/release.sh
+++ .travis/release.sh
@@ -7,6 +7,10 @@ VERSION='1.0.0'
 GH_REPO=$(basename `git rev-parse --show-toplevel`)
 GH_REMOTE=$(git remote -v | head -n 1 | sed "s/.*github.com\/\([a-zA-Z_-]*\)\/.*/\1/g")
 
+echo $GH_USER
+echo $GH_REPO
+echo $GH_REMOTE
+
 # Set up a bit of configuration
 git config --local user.name $GH_USER
 git config --local user.username $GH_USER
@@ -21,6 +25,8 @@ else
 fi
 git add .
 git commit -m "Travis Build $PLATFORM"
+
+echo $(git status)
 
 # Silent push to avoid the token to be shown in the console ^.^
 git push deploy release-$VERSION </dev/null >/dev/null 2>/dev/null
