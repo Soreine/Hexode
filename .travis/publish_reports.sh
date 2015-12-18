e7cbc955c937a2d334dd9ad41ca2a022b17bd013
KtorZ <matthias.benkort@gmail.com>
2015-12-18 01:08:36 +0100

    Add proper commit message for PR automated acceptance

--- .travis/publish_reports.sh
+++ .travis/publish_reports.sh
@@ -10,7 +10,13 @@ git add -f $REPORT_FILES
 git commit -m "Whatever"
 git checkout gh-pages
 git merge -X theirs --squash gh-pages-temp
-git commit -m "Travis Reports #$TRAVIS_BUILD_NUMBER"
+
+if [ $TRAVIS_PULL_REQUEST ]
+then
+    git commit -m "Travis reports PR#$TRAVIS_PULL_REQUEST"
+else
+    git commit -m "Travis Reports build#$TRAVIS_BUILD_NUMBER"
+fi
 
 # Silent push to avoid the token to be shown in the console ^.^
 git push deploy gh-pages </dev/null >/dev/null 2>/dev/null
