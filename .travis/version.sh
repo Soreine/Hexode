48e2eccc029a0a79f6fd57a7dbe0caf12c2c817b
KtorZ <matthias.benkort@gmail.com>
2015-12-21 10:10:43 +0100

    [PATCH] Fix versioning script

--- .travis/version.sh
+++ .travis/version.sh
@@ -1,42 +1,49 @@
 #!/bin/sh
 
-MAJORS=$(git log --grep="\[ *major *\]" --oneline)
+REGMAJ="^\[.*major\|MAJOR.*\]"
+REGMIN="^\[.*minor\|MINOR.*\]"
+REGPAT="^\[.*patch\|PATCH.*\]"
+REGLST="^[a-z0-9]+"
+REGCOM="[a-z0-9]+ \["
 
-LAST_MAJOR=$(echo $MAJORS | grep -oE "^[a-z0-9]+")
+MAJORS=$(git log --grep="$REGMAJ" --oneline)
+
+
+LAST_MAJOR=$(echo $MAJORS | grep -oE "$REGLST")
 if [ $LAST_MAJOR ]
 then
-    MINORS=$(git log --grep="\[ *minor *\]" --oneline $LAST_MAJOR..)
-    MAJOR=$(echo $MAJORS | grep -oE "[a-z0-9]+ \[" | wc -l)
+    MINORS=$(git log --grep="$REGMIN" --oneline $LAST_MAJOR..)
+    MAJOR=$(echo $MAJORS | grep -oE "$REGCOM" | wc -l)
 else
-    MINORS=$(git log --grep="\[ *minor *\]" --oneline)
+    MINORS=$(git log --grep="$REGMIN" --oneline)
     MAJOR=0
 fi
 
-LAST_MINOR=$(echo $MINORS | grep -oE "^[a-z0-9]+")
+LAST_MINOR=$(echo $MINORS | grep -oE "$REGLST")
 if [ $LAST_MINOR ]
 then
-    PATCHES=$(git log --grep="\[ *patch *\]" --oneline $LAST_MINOR..)
-    MINOR=$(echo $MINORS | grep -oE "[a-z0-9]+ \[" | wc -l)
+    PATCHES=$(git log --grep="$REGPAT" --oneline $LAST_MINOR..)
+    MINOR=$(echo $MINORS | grep -oE "$REGCOM" | wc -l)
 else
     if [ $LAST_MAJOR ]
     then
-        PATCHES=$(git log --grep="\[ *patch *\]" --oneline $LAST_MAJOR..)
+        PATCHES=$(git log --grep="$REGPAT" --oneline $LAST_MAJOR..)
     else
-        PATCHES=$(git log --grep="\[ *patch *\]" --oneline)
+        PATCHES=$(git log --grep="$REGPAT" --oneline)
     fi
     MINOR=0
 fi
 
-
-LAST_PATCH=$(echo $PATCHES | grep -oE "^[a-z0-9]+")
+LAST_PATCH=$(echo $PATCHES | grep -oE "$REGLST")
 if [ $LAST_PATCH ]
 then
-    PATCH=$(echo $PATCHES | grep -oE "[a-z0-9]+ \[" | wc -l)
+    PATCH=$(echo $PATCHES | grep -oE "$REGCOM" | wc -l)
 else
     PATCH=0
 fi
 
 VERSION=$MAJOR.$MINOR.$PATCH
+echo VERSION: $VERSION
 
 unset MAJORS
 unset MINORS
@@ -47,3 +54,8 @@ unset LAST_PATCH
 unset MAJOR
 unset MINOR
 unset PATCH
+unset REGMAJ
+unset REGMIN
+unset REGPAT
+unset REGCOM
+unset REGLST
